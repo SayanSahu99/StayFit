f5bc3315f546f1ff73bcfe0070ccaa0b8412a5cb
SayanSahu99 <sayansovan@gmail.com>
2021-02-20 00:32:51 +0530

    Styled the logout modal

--- src/screens/Logout/Logout.js
+++ src/screens/Logout/Logout.js
@@ -1,5 +1,6 @@
-import React, {useState} from 'react'
-import { View, Text, Button } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View } from 'react-native'
+import {Text, Button} from 'react-native-elements'
 import firebase from 'firebase';
 import Modal from 'react-native-modal';
 import { useSelector, useDispatch } from 'react-redux';
@@ -10,42 +11,54 @@ import Spinner from '../../components/activityIndicator';
 export default function Logout({ navigation }) {
 
   const isLoading = useSelector(state => state.auth.isLoading);
-  const [isModalVisible, setModalVisible] = useState(true);
   const dispatch = useDispatch();
+  const [isModelOpen, toggleModel] = useState(false);
 
-  const toggleModal = () => {
-    setModalVisible(!isModalVisible);
-  };
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      toggleModel(true);
+    });
+
+    // Return the function to unsubscribe from the event so it gets removed on unmount
+    return unsubscribe;
+  }, [navigation]);
 
   return (
-    <View style={styles.containerView}>
+    <View style={styles.centeredView}>
       {isLoading ? <Spinner /> : (
         <View>
 
-        <Modal isVisible={isModalVisible}>
-        <View style={{flex: 1}}>
-            <Text>Logout?</Text>
+          <Modal isVisible={isModelOpen}>
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                  <Text  style={styles.modalText}>Do You Want to Logout?</Text>
 
-            <Button title="NO" onPress={toggleModal} />
-            <Button title="YES" onPress={
-                () => {
-                    dispatch(requestLogout());
-                    firebase.auth().signOut().then(() => {
-                      dispatch(receiveLogout());
-                      console.log("user signed out");
-                      navigation.navigate('AuthNavigator');
-                    }).catch(function (error) {
-                      // An error happened.
-                      console.log(error);
-                      dispatch(logoutError(error));
-                    });
-                  }
-            } />
-        </View>
-        </Modal>
+                  <View style={{flexDirection:'row'}}>
+
+                    <Button containerStyle={{marginHorizontal: 20}} type="clear" title="NO" onPress={() => {
+                      toggleModel(false);
+                      navigation.goBack();
+                    }} />
+                    <Button containerStyle={{marginHorizontal: 20}} type="clear" title="YES" onPress={
+                      () => {
+                        dispatch(requestLogout());
+                        firebase.auth().signOut().then(() => {
+                          dispatch(receiveLogout());
+                          console.log("user signed out");
+                          navigation.navigate('AuthNavigator');
+                        }).catch(function (error) {
+                          // An error happened.
+                          console.log(error);
+                          dispatch(logoutError(error));
+                        });
+                      }
+                    } />
+                  </View>
+                </View>
+              </View>
+          </Modal>
         </View>
       )}
     </View>
-
   )
 }
