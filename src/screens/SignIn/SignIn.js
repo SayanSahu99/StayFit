745c4d09994fd6a99b8b5d9a08d37b8bac15972c
SayanSahu99 <sayansovan@gmail.com>
2021-03-07 01:31:17 +0530

    Removing twitter button and changing the other signing button UI

--- src/screens/SignIn/SignIn.js
+++ src/screens/SignIn/SignIn.js
@@ -1,54 +1,77 @@
 
-import React from 'react'
+import React, {useEffect} from 'react'
 import styles from "../Register/style";
 import {Keyboard, Text, View, TextInput, TouchableWithoutFeedback, Platform, KeyboardAvoidingView } from 'react-native';
 import { Button, SocialIcon } from 'react-native-elements';
+import { useSelector, useDispatch } from 'react-redux';
+import { loginUserFacebook, loginUserGoogle, requestLogin } from '../../Redux/ActionCreaters/auth'
+import Loader from '../../components/loading';
 
-export default function Register({ navigation }) {
+export default function SignIn({ navigation }) {
   // TODO: add firebase login function later
 
-  return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS == "ios" ? "padding" : "height"}
-      keyboardVerticalOffset={Platform.OS === 'ios' ? 40 : 0}
-      style={styles.containerView}
-    >
+  useEffect(() => {
+    return () => {
+      if (isAuthenticated) {
+        props.navigation.push('DetailsForm');
+      }
+    }
+  }, []);
 
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.loginScreenContainer}>
-          <View style={styles.loginFormView}>
-          <Text style={styles.logoText}>Stay Fit</Text>
-            <TextInput placeholder="Email" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} />
-            <TextInput placeholder="Password" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} secureTextEntry={true}/>
-            <Button
-              buttonStyle={styles.loginButton}
-              onPress={() => {}}
-              title="LOGIN"
-            />
-            <View style={styles.socialView}>
-              <View style={styles.socialTextView}><Text style={{fontSize: 15}}>Or login with</Text></View>
-              
-              <View style={styles.socialButtonView}>
-                <SocialIcon
-                  type='facebook'
-                  buttonStyle={styles.fbLoginButton}
-                  onPress={() =>{}}
-                />
-                <SocialIcon
-                  type='google'
-                  buttonStyle={styles.fbLoginButton}
-                  onPress={() =>{}}
-                />
-                <SocialIcon
-                  type='twitter'
-                  buttonStyle={styles.fbLoginButton}
-                  onPress={() =>{}}
-                />
+  const isLoading = useSelector(state => state.auth.isLoading);
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+
+  return (
+    <View style={styles.containerView}>
+
+      <Loader loading={isLoading} />
+      <KeyboardAvoidingView
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.OS === 'ios' ? 40 : 0}
+        style={styles.containerView}
+      >
+
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.loginScreenContainer}>
+            <View style={styles.loginFormView}>
+              <Text style={styles.logoText}>Stay Fit</Text>
+              <TextInput placeholder="Email" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} />
+              <TextInput placeholder="Password" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} secureTextEntry={true} />
+              <Button
+                buttonStyle={styles.loginButton}
+                onPress={() => { }}
+                title="SIGN UP"
+              />
+              <View style={styles.socialView}>
+                <View style={styles.socialTextView}><Text style={{ fontSize: 15 }}>Or connect with</Text></View>
+
+                <View style={styles.socialButtonView}>
+                  <SocialIcon
+                    type='facebook'
+                    button
+                    title='Sign In With Facebook'
+                    onPress={() => { 
+                      dispatch(requestLogin());
+                      dispatch(loginUserFacebook());
+                    }}
+                  />
+                  <SocialIcon
+                    type='google'
+                    button
+                    title='Sign In With Google'
+                    onPress={() => {
+                      dispatch(requestLogin());
+                      dispatch(loginUserGoogle());
+                    }}
+                  />
+                </View>
               </View>
             </View>
           </View>
-        </View>
-      </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
+        <View style={{alignSelf:"stretch"}}></View>
+      </KeyboardAvoidingView>
+    </View>
   )
 }
